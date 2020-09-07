4e89afe0e704143ae098109dbf06103080fd14c9
jordansu <jdsu1996@gmail.com>
2020-09-07 11:08:19 +0800

    Updates

---
+++ precache-manifest.9afac7c1a2bf490594dce76c4776c4b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0151249151d8b4d884ae01bf7b4e9a5",
+    "url": "/Testing2/index.html"
+  },
+  {
+    "revision": "ee15962cbf7bb162e857",
+    "url": "/Testing2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/Testing2/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Testing2/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee15962cbf7bb162e857",
+    "url": "/Testing2/static/js/main.02d215d8.chunk.js"
+  },
+  {
+    "revision": "e8a91849839080154064",
+    "url": "/Testing2/static/js/runtime-main.e55765e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Testing2/static/media/logo.5d5d9eef.svg"
+  }
+]);
